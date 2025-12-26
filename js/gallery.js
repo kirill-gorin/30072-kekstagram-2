815e3621d58eed1cd55fc9f763d6b17747c84516
Kirill Gorin <kirillpwnography@gmail.com>
2025-12-26 09:41:51 +0100

    create galley

---
+++ js/gallery.js
@@ -0,0 +1,21 @@
+import { generatePhotos } from './data.js';
+
+const photos = generatePhotos();
+
+export const createGalley = () => {
+  const photoTemplate = document.querySelector('#picture').content;
+  const photoWrapper = document.querySelector('.pictures');
+  const photoFragment = document.createDocumentFragment();
+
+  photos.forEach((photo) => {
+    const photoTemplateCloned = photoTemplate.cloneNode(true);
+    photoTemplateCloned.querySelector('.picture__img').src = photo.url;
+    photoTemplateCloned.querySelector('.picture__img').alt = photo.description;
+    photoTemplateCloned.querySelector('.picture__comments').textContent = photo.comments.length;
+    photoTemplateCloned.querySelector('.picture__likes').textContent = photo.likes;
+    photoFragment.append(photoTemplateCloned);
+  });
+
+  photoWrapper.append(photoFragment);
+};
+
--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-import { generatePhotos } from './data.js';
+import { createGalley } from './gallery.js';
 
-generatePhotos();
+createGalley();
