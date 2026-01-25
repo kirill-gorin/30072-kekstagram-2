e7a095dd3511b613a1cc9f74610fc0727956244b
Kirill Gorin <kirillpwnography@gmail.com>
2026-01-25 09:58:52 +0100

    refactor by PR comments

--- js/photo-form.js
+++ js/photo-form.js
@@ -1,3 +1,8 @@
+const COMMENT_MAX_LENGTH = 140;
+
+const HASHTAGS_MAX_AMOUNT = 5;
+const HASHTAG_MAX_LENGTH = 20;
+
 const bodyElement = document.querySelector('body');
 const uploadInput = document.querySelector('.img-upload__input');
 const modalOverlay = document.querySelector('.img-upload__overlay');
@@ -8,46 +13,45 @@ const imageForm = document.querySelector('.img-upload__form');
 const hashtagsInput = document.querySelector('.text__hashtags');
 const commentTextarea = document.querySelector('.text__description');
 
-const COMMENT_MAX_LENGTH = 140;
-
-const HASHTAGS_MAX_AMOUNT = 5;
-const HASHTAG_MAX_LENGTH = 20;
-
-const openModal = () => {
-  bodyElement.classList.add('modal-open');
-  modalOverlay.classList.remove('hidden');
-};
-
-const closeModal = () => {
-  bodyElement.classList.remove('modal-open');
-  modalOverlay.classList.add('hidden');
-  uploadInput.value = '';
-  imageForm.reset();
-};
-
-const handleUploadInputClick = (evt) => {
-  evt.preventDefault();
-  openModal();
-};
-
-const handleCloseButtonClick = (evt) => {
-  evt.preventDefault();
-  closeModal();
-};
-
-const handleCloseModalKeydown = (evt) => {
-  if (evt.key === 'Escape') {
-    if (!(document.activeElement === hashtagsInput || document.activeElement === commentTextarea)) {
-      closeModal();
-    }
-  }
-};
-
 const pristine = new Pristine(imageForm, {
   classTo: 'img-upload__field-wrapper',
   errorClass: 'img-upload__field-wrapper--error',
   errorTextParent: 'img-upload__field-wrapper'
 });
+
+const openModal = () => {
+  bodyElement.classList.add('modal-open');
+  modalOverlay.classList.remove('hidden');
+
+  closeModalButton.addEventListener('click', handleCloseButtonClick);
+  document.addEventListener('keydown', handleCloseModalKeydown);
+};
+
+const closeModal = () => {
+  bodyElement.classList.remove('modal-open');
+  modalOverlay.classList.add('hidden');
+  imageForm.reset();
+  pristine.reset();
+  document.removeEventListener('keydown', handleCloseModalKeydown);
+};
+
+function handleUploadInputClick (evt) {
+  evt.preventDefault();
+  openModal();
+}
+
+function handleCloseButtonClick (evt) {
+  evt.preventDefault();
+  closeModal();
+}
+
+function handleCloseModalKeydown (evt) {
+  if (evt.key === 'Escape') {
+    if (!(document.activeElement === hashtagsInput || document.activeElement === commentTextarea)) {
+      closeModal();
+    }
+  }
+}
 
 const checkHashtagValidity = () => {
   if (!hashtagsInput.value) {
@@ -55,7 +59,7 @@ const checkHashtagValidity = () => {
   }
 
   const purifiedInputValue = hashtagsInput.value.trim().toLowerCase();
-  const hashtagsArray = purifiedInputValue.split(/\s+/);
+  const hashtagsArray = purifiedInputValue.split(/\s+/).filter(Boolean);
   const uniqueHashtagsArray = new Set(hashtagsArray);
 
   if (uniqueHashtagsArray.size !== hashtagsArray.length) {
@@ -87,10 +91,6 @@ export const submitImageForm = () => {
   pristine.addValidator(commentTextarea, checkCommentValidity);
 
   uploadInput.addEventListener('change', handleUploadInputClick);
-
-  closeModalButton.addEventListener('click', handleCloseButtonClick);
-
-  document.addEventListener('keydown', handleCloseModalKeydown);
 
   imageForm.addEventListener('submit', (evt) => {
     evt.preventDefault();
--- js/photo-modal.js
+++ js/photo-modal.js
@@ -61,14 +61,19 @@ const createFullPhoto = (imageId, photos) => {
 const openModal = () => {
   photoModal.classList.remove('hidden');
   bodyElement.classList.add('modal-open');
+
+  photoModalCloseButton.addEventListener('click', handleCloseButtonClick);
+  document.addEventListener('keydown', handleCloseButtonKeydown);
 };
 
 const closeModal = () => {
   photoModal.classList.add('hidden');
   bodyElement.classList.remove('modal-open');
+
+  document.removeEventListener('keydown', handleCloseButtonKeydown);
 };
 
-const handleGalleryImageClick = (evt, photos) => {
+function handleGalleryImageClick (evt, photos) {
   const currentElement = evt.target;
   if (currentElement.classList.contains('picture__img')) {
     evt.preventDefault();
@@ -77,21 +82,19 @@ const handleGalleryImageClick = (evt, photos) => {
 
     openModal();
   }
-};
+}
 
-const handleCloseButtonClick = (evt) => {
+function handleCloseButtonClick (evt) {
   evt.preventDefault();
   closeModal();
-};
+}
 
-const handleCloseButtonKeydown = (evt) => {
+function handleCloseButtonKeydown (evt) {
   if (evt.key === 'Escape') {
     closeModal();
   }
-};
+}
 
 export const managePhotoModal = (photos) => {
   document.addEventListener('click', (evt) => handleGalleryImageClick(evt, photos));
-  photoModalCloseButton.addEventListener('click', handleCloseButtonClick);
-  document.addEventListener('keydown', handleCloseButtonKeydown);
 };
